885b303c689599419ac76f0def9296480dd2c41b
Leocroce <leo_croce@hotmail.com>
2022-02-04 11:43:42 -0300

    Adição do módulo inquirer

---
+++ modulo_inquirer.js
@@ -0,0 +1,17 @@
+module.exports = {
+    soma(a, b) {
+        console.log(`O resultado da soma é ${ a + b}`)
+    },
+
+    subtracao(a, b) {
+        console.log(`O resultado da subtração é ${ a - b}`)
+    },
+
+    multiplicacao(a, b) {
+        console.log(`O resultado da multiplicação é ${ a * b}`)
+    },
+
+    divisao(a, b) {
+        console.log(`O resultado da divisão é ${ a / b}`)
+    }    
+}
